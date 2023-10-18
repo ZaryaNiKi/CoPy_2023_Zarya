35a535523e4519abcfae34a2c75e11e4674b799b
ZaryaNiKi <ZaryaNiKonst@ya.ru>
2023-10-18 18:17:19 +0700

    Lecture06
    Lecture06

--- Lecture06/z1 (3).py
+++ Lecture06/z1 (3).py
@@ -1,7 +1,6 @@
-
 ## Некоторые начальные данные
-a = -3
-b = 2
+a = -5
+b = 5
 def Line(x): return 2*x
 def Parabola(x): return x**2
 #############################
@@ -10,9 +9,12 @@ def Parabola(x): return x**2
 def Def(func, x):
         eps = 10**(-5) ## Точность производной
         return (func(x+eps)-func(x))/(eps)
+def TwoDef(func , x):
+        eps = 10**(-5) ## Точность второй производной
+        return (Def(func, x+eps)-Def(func, x))/(eps)
 
 def Extremum(func, a, b):
-    step = 10**(-3)## Точность
+    step = 10**(-5)## Точность
 
     X_value = [ii*step for ii in range(int(a/step),int(b/step))]
     extr = [ii for ii in X_value if (Def(func, ii - step)*Def(func, ii) < 0)]
@@ -20,9 +22,9 @@ def Extremum(func, a, b):
     if extr == []:
        print(None)
     else:
-       type_extr = ["min" if Def(func, Def(func, ii)) > 0 else "max" for ii in extr]
+       type_extr = ["min" if TwoDef(func, ii) > 0 else "max" for ii in extr]
        [print(f"Для функции есть точка {extr[ii]} с погрешностью {step} -- это экстремум ({type_extr[ii]})") for ii in range(len(extr))];
-    
+
 
 Extremum(Line, a, b)
 Extremum(Parabola, a, b)
